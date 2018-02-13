a6ba039c90978ce7a396393c5899b7398a42097e
wyattfry <wyattfry@gmail.com>
2018-02-13 12:19:31 -0800

    Implement core function, toCSV(json)

---
+++ challenge_2/README.md
@@ -0,0 +1,21 @@
+# Mini-Apps
+## Challenge 2: CSV Report Generator
+
+
+### Components
+- __server.js:__ node / express server script, serves the files in the ``client`` folder, converts JSON to CSV
+- __client/app.js:__ handles submitting JSON to server, should receive a CSV-formatted response and display on same page
+- __client/index.html:__ static front-end page
+
+__Example Data__
+
+- samples/csv_report.csv
+- samples/sales_report.json
+
+### TODO
+* [ ] create a ``package.json`` to store dependencies
+* [ ] prevent default (page reload) when user submits data
+
+### Diagram
+![diagram1](https://github.com/wyattfry/images/blob/master/json-to-csv-diagram.png?raw=true)
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,36 @@
+var toCSV = function(json) {
+  let input = JSON.parse(json);
+  let output = "";
 
+  // first row: headers
+  // second row +: data
+  let headers = [];
+  for (let key in input) {
+    if (key !== "children") {
+      headers.push(key);
+    }
+  }
 
+  output += headers.join() + "\n";
+
+  let getProps = function(obj) {
+    let props = [];
+    for (let key in obj) {
+      if (key === "children") {
+      } else {
+        props.push(obj[key]);
+      }
+    }
+    return props;
+  };
+
+  let traverse = function(obj) {
+    output += getProps(obj).join() + "\n";
+    for (let i = 0; i < obj.children.length; i++) {
+      traverse(obj.children[i]);
+    }
+  };
+
+  traverse(input, output);
+  return output;
+};
