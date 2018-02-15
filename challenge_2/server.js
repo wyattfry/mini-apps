6152848f9921384afe9add91dbd92acbf48e7384
wyattfry <wyattfry@gmail.com>
2018-02-14 21:16:37 -0800

    I forget what I did

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,7 +5,7 @@ var toCSV = require('./toCSV');
 var morgan = require('morgan');
 var parser = require('body-parser');
 
-app.use(express.static('client/'));
+app.use(express.static('client/dist'));
 app.use(parser.json());
 
 app.post('/', function(req, res) {
